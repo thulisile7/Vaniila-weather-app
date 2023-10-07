b0be3b3e2a921bf047ba5398e4fc2900564fc540
Nomcebo <146365149+thulisile7@users.noreply.github.com>
2023-10-07 14:59:18 +0200

    current icon updated with API

--- index.js
+++ index.js
@@ -29,14 +29,20 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 
 let apiKey = "eaaaa2ab0aed7ae98ae7f4db7b3340f1";
-let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=Observatory&appid=${apiKey}&units=metric`;
+let city = "Sandton";
+let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiURL).then(displayTemperature);

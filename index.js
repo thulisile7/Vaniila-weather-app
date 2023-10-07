61d510b82050172f00b7dcb73282aa21d0432a08
Nomcebo <146365149+thulisile7@users.noreply.github.com>
2023-10-07 12:48:20 +0200

    show current temperature, wind speed,humidity  and weather description

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "eaaaa2ab0aed7ae98ae7f4db7b3340f1";
+let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=Observatory&appid=${apiKey}&units=metric`;
+axios.get(apiURL).then(displayTemperature);
